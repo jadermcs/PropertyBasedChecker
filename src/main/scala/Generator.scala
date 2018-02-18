d3f164919c8917694dc381b34c87e8fea1a35653
Jader Martins <jadermcs94@gmail.com>
2018-02-17 23:02:08 -0300

    init

---
+++ src/main/scala/Generator.scala
@@ -0,0 +1,14 @@
+object Generator {
+  trait Prop {
+    def &&(p: Prop): Prop
+
+    def ||(p: Prop): Prop
+  }
+
+  def listOf[A](a: Gen[A]): Gen[List[A]]
+
+  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]]
+
+  def forAll[A](a: Gen[A])(f: A => Boolean): Prop
+
+}
